4bf3eb5428a4e9e54bd7ee502a2e4c2de5ef64ac
shareed <shannontlreed@gmail.com>
2024-01-15 22:39:20 -0800

    updating home

--- src/components/home/home-content/HomeMD.js
+++ src/components/home/home-content/HomeMD.js
@@ -2,42 +2,36 @@ import { Grid, Box, Button, Typography } from '@mui/material';
 
 const HomeMD = () => {
     return (
-        <Grid container sx={{}}>
-            <Grid item md={7} sx={{ color: 'red', height: '100vh' }} mt={'8rem'} pt={'2rem'}>
-                <Box sx={{}} mr={'1rem'} ml={'1rem'} >
-                    <Typography variant='h3' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
-                        Shannon Reed
-                    </Typography>
-                </Box>
-                <Box fontFamily={'"Lemon", serif;'} m={'3rem'} mb={'2rem'} mt={'2rem'} textAlign={'center'}>
-                    <Typography sx={{
-                        color: '#50424F'
-                    }}
-                        variant='p'
-                    >
-                        Software Engineer located Las Vegas, Nevada. 
-                        I specialize in writing clean elegant code that brings 
-                        magnificent user friendly designs to life. Holding a 
-                        Bachelor’s Degree in Information Technology and Web Development,
-                        for 4+ years I have worked with Front-End, Back-End, UX/UI and BPM Software.
-                    </Typography>
-                </Box>
-
-                <Box display={'flex'} justifyContent={'space-between'} m={'7rem'} mb={'2rem'} mt={'0'}>
-                    <Button sx={{ backgroundColor: 'rgb(179, 10, 179)', color: '#FFFFFF', p: '1rem' }}  >
-                        <Typography variant='p'>
-                            Start Learning
+        <Grid container >
+            <Grid item md={7} sx={{ height: '100vh' }} mt={'8rem'} pt={'2rem'} >
+            <Grid container ml={'3rem'} >
+                    <Grid item sx={12} >
+                        <Typography variant='h4' textAlign={'center'} color={'white'} fontFamily={'"Lemon", serif'} >
+                            Hi I'm MD
                         </Typography>
-
-                    </Button>
-
-                    <Button sx={{ backgroundColor: '#50424F', color: '#FFFFFF', p: '1rem' }}>
-                        <Typography variant='p'>
-                            View Templates
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item sx={12} ml={'4rem'}  >
+                        <Typography variant='h3' color={'#50424F'} fontFamily={'"Lemon", serif'} >
+                            Shannon Reed
                         </Typography>
-
-                    </Button>
-                </Box>
+                        <Typography variant='h3' mt={'-3.4rem'} color={'#B7A6B5'} fontFamily={'"Lemon", serif'} >
+                            Shannon Reed
+                        </Typography>
+                        <Typography variant='h3' mt={'-3.4rem'} color={'#B30AB3'} fontFamily={'"Lemon", serif'} >
+                            Shannon Reed
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item sx={12} ml={'4rem'} >
+                        <Typography variant='h5' color={'white'} fontFamily={'"Lemon", serif'} >
+                            Web Developer and Designer
+                        </Typography>
+                    </Grid>
+                </Grid>
+                
             </Grid>
             <Grid item md={5} sx={{ backgroundColor: '#50424F', height: '80vh' }} mt={'3rem'} pt={'2rem'}>
                 <Box backgroundColor={'rgb(179, 10, 179)'} m={'1rem'} p={'1rem'} height={'35vh'}>
