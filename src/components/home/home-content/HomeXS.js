c49a3a00deeaec7616b71e4a91e3a359a550f4d6
shareed <shannontlreed@gmail.com>
2024-01-15 20:32:01 -0800

    changing home section

--- src/components/home/home-content/HomeXS.js
+++ src/components/home/home-content/HomeXS.js
@@ -3,27 +3,40 @@ import { NavLink } from 'react-router-dom';
 const HomeXS = () => {
     return (
         <Box sx={{ height: '100vh', paddingTop: '10rem' }} >
-            <Grid container   >
-                <Grid item sx={12} m={'auto'} width={'57%'}>
-                        <Typography variant='h4' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
-                            Shannon Reed
+            <Grid container ml={'3rem'}>
+                <Grid item sx={12} >
+                        <Typography variant='h3' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
+                            Hi I'm
                         </Typography>
                 </Grid>
+                <Grid item sx={12} ml={'2rem'} >
+                        <Typography variant='h2' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
+                            Shannon 
+                        </Typography>
+                </Grid>
+                <Grid item sx={12} ml={'3rem'} >
+                        <Typography variant='h4' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
+                            and I
+                        </Typography>
+                </Grid>
+                {/* <Grid item sx={12} m={'auto'} width={'57%'}>
+                        <Typography variant='h4' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
+                            Think
+                        </Typography>
+                </Grid>
+                <Grid item sx={12} m={'auto'} width={'57%'}>
+                        <Typography variant='h4' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
+                            Design
+                        </Typography>
+                </Grid>
+                <Grid item sx={12} m={'auto'} width={'57%'}>
+                        <Typography variant='h4' color={'white'} textAlign={'center'} fontFamily={'"Lemon", serif'} >
+                            Create
+                        </Typography>
+                </Grid> */}
             </Grid>
 
-            <Box fontFamily={'"Lemon", serif;'} m={'2rem'} textAlign={'center'}>
-                <Typography sx={{
-                    color: '#50424F'
-                }}
-                    variant='p'
-                >
-                    Software Engineer located Las Vegas, Nevada. 
-                        I specialize in writing clean elegant code that brings 
-                        magnificent user friendly designs to life. Holding a 
-                        Bachelor’s Degree in Information Technology and Web Development,
-                        for 4+ years I have worked with Front-End, Back-End, UX/UI and BPM Software.
-                </Typography>
-            </Box>
+           
 
             <Box display={'flex'} flexDirection={'column'} m={'2rem'} ml={'7rem'} mr={'7rem'} >
             <NavLink  to={'/designs'}>
--- src/components/navigation/NavLogo.js
+++ src/components/navigation/NavLogo.js
@@ -2,27 +2,29 @@ import { Box, Typography } from '@mui/material';
 import logoS from '../../logo-s.svg'
 import logoS2 from '../../logo-s2.svg'
 import logoS3 from '../../logo-s3.svg'
+import { NavLink } from 'react-router-dom';
 
 const NavLogo = () => {
     return (
         <Box textAlign={'center'} color={'rgb(179, 10, 179)'}>
+            <NavLink to={'/'}>
             <Box>
-            <Typography >
-                <img src={logoS3} alt="" width={'60rem'}/>
-            </Typography>
+                <Typography >
+                    <img src={logoS3} alt="" width={'60rem'} />
+                </Typography>
             </Box>
             <Box mt={'-4rem'}>
-            <Typography >
-                <img src={logoS2} alt="" width={'60rem'}/>
-            </Typography>
+                <Typography >
+                    <img src={logoS2} alt="" width={'60rem'} />
+                </Typography>
             </Box>
 
             <Box mt={'-4rem'}>
-            <Typography >
-                <img src={logoS} alt="" width={'60rem'}/>
-            </Typography>
+                <Typography >
+                    <img src={logoS} alt="" width={'60rem'} />
+                </Typography>
             </Box>
-            
+            </NavLink>
         </Box>
     )
 }
